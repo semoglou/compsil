1c8bc50377bb83b5e4b1cdb2b9a77ea437c71835
Aggelos Semoglou <a.semoglou@outlook.com>
2026-09-08 10:50:53 +0200

    Bump version from 0.1.2 to 0.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="compsil",
-    version="0.1.2",
+    version="0.1.3",
     author="Aggelos Semoglou",
     author_email="a.semoglou@outlook.gr",
     description="CompSil: Composite Silhouette for Cluster-Count Selection",
